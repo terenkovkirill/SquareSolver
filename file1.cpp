92ad8beb001ab321acdabe74a6303e5bbc9b6168
terenkovkirill <terenkovkirill@yandex.ru>
2024-08-21 21:29:52 +0300

    Работает без тестов, есть небольшая ошибка

--- file1.cpp
+++ file1.cpp
@@ -2,68 +2,107 @@
 #include <stdio.h>
 #include <math.h>
 
-int main()
+const float E = 0.001;
+const int NO_ROOTS = 0;
+const int ONE_ROOT = 1;
+const int TWO_ROOTS = 2;
+const int INFINITY_ROOTS = 3;
+
+int Compare_Zero(float number)
 {
-    float a, b, c, x1, x2, discr, E;
-    int nRoots;
-    scanf ("%g %g %g", &a, &b, &c) ;
-    discr = b*b - 4*a*c;
-    E = 0.001;
-    x1 = x2 = 0;
-    nRoots = SolveSquare (a, b, c, discr, &x1, &x2);
-    switch()
+    if (abs(number) < E)
     {
-        case 0: printf ("No solutions \n");
+        return 1;                //дискриминант < 0, нет корней
+    }
+    else
+    {
+        return 0;
+    }
+
+}
+
+int SolveSquare (float a, float b, float c, float *adrs_x1, float *adrs_x2)  //функция счёта корней
+{
+    float discr;
+    discr = b*b - 4*a*c;
+    if (Compare_Zero(discr))
+    {
+        return - b / (2*a);
+    }
+    else if (discr < 0)
+    {
+        return NO_ROOTS;                //дискриминант < 0, нет корней
+    }
+    else
+    {
+        if (Compare_Zero(a))
+        {
+            if (Compare_Zero(b))
+            {
+                if (Compare_Zero(c))
+                {
+                    return INFINITY_ROOTS;         //a = b = c = 0, любой корень
+                }
+                else
+                {
+                    return NO_ROOTS;         //a = 0, b = 0, c != 0, нет корней
+                }
+            }
+            else
+            {
+                *adrs_x1 = - c / b;
+                return ONE_ROOT;          //a = 0, b != 0,  один корень
+            }
+
+        }
+        else
+        {
+            *adrs_x1 = (- b + sqrt(discr)) / (2*a);
+            *adrs_x2 = (-b - sqrt(discr)) / (2*a);
+            return TWO_ROOTS;             //a != 0  два корня
+        }
+    }
+}
+
+void printRoots(int nRoots, float x1, float x2)
+{
+    switch(nRoots)                     /*перебор вариантов переменной nRoots*/
+    {
+        case NO_ROOTS: printf ("No solutions \n");
                 break;
-        case 1: printf ("%g \n", x1);
+        case ONE_ROOT: printf ("%g \n", x1);
                 break;
-        case 2: printf ("%g %g \n", x1, x2);
+        case TWO_ROOTS: printf ("%g %g \n", x1, x2);
                 break;
-        case 3: printf ("Any solution \n");
+        case INFINITY_ROOTS: printf ("Any solution \n");
                 break;
         default:    printf ("main(): ERROR: nRoots = %d \n", nRoots);
                     break;
     }
 }
 
-
-int SolveSquare(a, b, c, discr, *x1, *x2)  //функция счёта корней
-float a, b, c, discr;
-int *x1, *x2;
+int main(void)
 {
-    if (abs(discr) < E)
-    {
-        return 0;                //дискриминант < 0, нет корней
-    }
-    else
-    {
-        if (abs(a) - 0 < E)
-        {
-            if (abs(b) - 0 < E)
-            {
-                if (abs(c) - 0 < E)
-                {
-                    return 3;         //a = b = c = 0, любой корень
-                }
-                else
-                {
-                    return 0;         //a = 0, b = 0, c != 0, нет корней
-                }
-            }
-            else
-            {
-                *x1 = - c / b
-                return 1;          //a = 0, b != 0,  один корень
-            }
+    float a, b, c, x1, x2;
+    int nRoots;
+    scanf ("%g %g %g", &a, &b, &c) ;
+    x1 = x2 = 0;
+    nRoots = SolveSquare (a, b, c, &x1, &x2);
+    printRoots(nRoots, x1, x2);
+}
 
-        }
-        else
-        {
-            *x1 = (- b + sqrt(discr)) / (2*a);
-            *x2 = (-b - sqrt(discr)) / (2*a);
-            return 2;             //a != 0  два корня
-        }
-    }
+
+
+
+
+
+
+
+int CheckTest(int nTest, float a, float b, float c, float x1Expected, float x2Expected, int nRootsExpected)
+{
+    float x1 = 0, x2 = 0;
+    int nRoots = SolveSquare (a, b, c, &x1, &x2);
+    //if (nRoots != nRootsExpected
 }
 
 
