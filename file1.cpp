c5263d0acdbea824a470ca99112c39045e045945
terenkovkirill <terenkovkirill@yandex.ru>
2024-08-22 19:04:31 +0300

    Работает со структурой и недочётами

--- file1.cpp
+++ file1.cpp
@@ -2,35 +2,43 @@
 #include <stdio.h>
 #include <math.h>
 
-const float E = 0.001;
+const float Eps = 0.0001;
 const int NO_ROOTS = 0;
 const int ONE_ROOT = 1;
 const int TWO_ROOTS = 2;
 const int INFINITY_ROOTS = 3;
 
-int Compare_Num(float num1, float num2)
+struct CheckData
 {
-    if (abs(num1 - num2) < E)
-    {
-        return 1;                //дискриминант < 0, нет корней
-    }
-    else
-    {
-        return 0;
-    }
+    double a, b, c;
+    double x1Expected, x2Expected;
+    int nRootsExpected, nTest;
+};
 
+
+int CheckTest(CheckData test);
+int CompareNum(double num1, double num2);
+int SolveSquare (double a, double b, double c, double *adrsx1, double *adrsx2);
+void printRoots(int nRoots, double x1, double x2);
+
+
+int CompareNum(double num1, double num2)
+{
+    return (fabs(num1 - num2) < Eps);
 }
 
 
-int SolveSquare (float a, float b, float c, float *adrs_x1, float *adrs_x2)  //функция счёта корней
+int SolveSquare (double a, double b, double c, double *adrsx1, double *adrsx2)  //функция счёта корней
 {
-    float discr;
+    // TODO: написать проверки через assert для указателей
+    double discr;
     discr = b*b - 4*a*c;
-    if (Compare_Num(a, 0))
+    if (CompareNum(a, 0))
     {
-        if (Compare_Num(b, 0))
+        if (CompareNum(b, 0))
         {
-            if (Compare_Num(c, 0))
+        // TODO: прочитать что такое тернарный оператор
+            if (CompareNum(c, 0))
             {
                 return INFINITY_ROOTS;         //a = b = c = 0, любой корень
             }
@@ -41,16 +49,16 @@ int SolveSquare (float a, float b, float c, float *adrs_x1, float *adrs_x2)  //ф
         }
         else
         {
-            *adrs_x1 = - c / b;
+            *adrsx1 = - c / b;
             return ONE_ROOT;          //a = 0, b != 0,  один корень
         }
 
     }
     else
     {
-        if (Compare_Num(discr, 0))
+        if (CompareNum(discr, 0))
         {
-            *adrs_x1 = - b / (2*a);
+            *adrsx1 = - b / (2*a);
             return ONE_ROOT;
         }
         else if (discr < 0)
@@ -59,15 +67,15 @@ int SolveSquare (float a, float b, float c, float *adrs_x1, float *adrs_x2)  //ф
         }
         else
         {
-            *adrs_x1 = (- b + sqrt(discr)) / (2*a);
-            *adrs_x2 = (-b - sqrt(discr)) / (2*a);
+            *adrsx1 = (- b + sqrt(discr)) / (2*a);
+            *adrsx2 = (-b - sqrt(discr)) / (2*a);
             return TWO_ROOTS;             //a != 0  два корня
         }
     }
 }
 
 
-void printRoots(int nRoots, float x1, float x2)
+void printRoots(int nRoots, double x1, double x2)
 {
     switch(nRoots)                     /*перебор вариантов переменной nRoots*/
     {
@@ -87,16 +95,16 @@ void printRoots(int nRoots, float x1, float x2)
 
 //Начало тестирования
 
-int CheckTest(int nTest, float a, float b, float c, float x1Expected, float x2Expected, int nRootsExpected)
+int CheckTest(CheckData test)   //функция тестирования
 {
-    float x1 = 0, x2 = 0;
-    int nRoots = SolveSquare (a, b, c, &x1, &x2);
-    if (nRoots != nRootsExpected || not(Compare_Num(x1, x1Expected)) || not(Compare_Num(x2, x2Expected))
+    double x1 = 0, x2 = 0;
+    int nRoots = SolveSquare (test.a, test.b, test.c, &x1, &x2);
+    if (nRoots != test.nRootsExpected || !(CompareNum(x1, test.x1Expected)) || !(CompareNum(x2, test.x2Expected)))
     {
         printf ("Error Test %d, a = %g, b = %g, c = %g, x1 = %g, x2 = %g, nRoot = %d \n"
                 "Expected: x1 = %g, x2 = %g, nRoots = %d \n",
-                nTest, a, b, c, x1, x2, nRoots,
-                x1Expected, x2Expected, nRootsExpected);
+                test.nTest, test.a, test.b, test.c, x1, x2, nRoots,
+                test.x1Expected, test.x2Expected, test.nRootsExpected);
     }
     else
     {
@@ -104,23 +112,37 @@ int CheckTest(int nTest, float a, float b, float c, float x1Expected, float x2Ex
     }
 }
 
+void AllTests()
+{
+    struct CheckData test1 = {.a = 1.0, .b = 2.0, .c = 1.0, .x1Expected = -1.0, .x2Expected = 0.0, .nRootsExpected = ONE_ROOT, .nTest = 1};
+    struct CheckData test2 = {.a = 1.0, .b = 2.0, .c = 3.0, .x1Expected = 0.0, .x2Expected = 0.0, .nRootsExpected = NO_ROOTS, .nTest = 2};
+    struct CheckData test3 = {.a = 0.0, .b = 2.0, .c = 1.0, .x1Expected = -0.5, .x2Expected = 0.0, .nRootsExpected = ONE_ROOT, .nTest = 3};
+    struct CheckData test4 = {.a = 1.0, .b = 3.0, .c = 2.0, .x1Expected = -1.0, .x2Expected = -2.0, .nRootsExpected = TWO_ROOTS, .nTest = 4};
+    struct CheckData test5 = {.a = 0.0, .b = 0.0, .c = 0.0, .x1Expected = 0.0, .x2Expected = 0.0, .nRootsExpected = INFINITY_ROOTS, .nTest = 5};
+    struct CheckData test6 = {.a = 0.0, .b = 0.0, .c = 5.0, .x1Expected = 0.0, .x2Expected = 0.0, .nRootsExpected = NO_ROOTS, .nTest = 6};
+    CheckTest(test1);
+    CheckTest(test2);
+    CheckTest(test3);
+    CheckTest(test4);
+    CheckTest(test5);
+    CheckTest(test6);
+}
+
+
 //Конец тестирования
 
 int main(void)
 {
-    float a, b, c, x1, x2;
+    double a, b, c, x1, x2;
     int nRoots;
-    scanf ("%g %g %g", &a, &b, &c) ;
+    // TODO: написать безопасную функцию для ввода чисел
+    int x = scanf ("%lg %lg %lg", &a, &b, &c) ;
     x1 = x2 = 0;
     nRoots = SolveSquare (a, b, c, &x1, &x2);
     printRoots (nRoots, x1, x2);
-    CheckTest(0, 1.0, 2.0, 1.0, -1.0, 0.0, ONE_ROOT);
-    CheckTest(1, 1.0, 2.0, 3.0, 0.0, 0.0, NO_ROOTS);
-    CheckTest(2, 0.0, 2.0, 1.0, -0.5, 0.0, ONE_ROOT);
-    CheckTest(3, 1.0, 3.0, 2.0, -1.0, -2.0, TWO_ROOTS);
-    CheckTest(4, 0.0, 0.0, 0.0, 0.0, 0.0, INFINITY_ROOTS);
-    CheckTest(5, 0.0, 0.0, 5.0, 0.0, 0.0, NO_ROOTS);
-    float a1 = 0;
-    a1 = a1 * 4;
-    printf ("%g", a1);
+    AllTests();
 }
+
+//нужно объявить переменные вначале
+// TODO: выбрать один способ написания имен функций и переменных
+//функция enum
