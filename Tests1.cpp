aa49fbb44e316e31f68b8bfaf1cd291b521506dc
Теренков Кирилл <terenkovkirill@yandex.ru>
2024-08-26 20:22:18 +0300

    LittleChanges40

--- Tests1.cpp
+++ Tests1.cpp
@@ -7,7 +7,6 @@
 void RunTest (const struct CheckData *adrs_test)   //функция тестирования
 {
     double x1 = 0, x2 = 0;
-    // test->a = 10;
     int nRoots = SolveSquare ((adrs_test->a, adrs_test->b, adrs_test->c, &x1, &x2);
     if (nRoots != adrs_test->nRootsExpected || !(CompareNum(x1, adrs_test->x1Expected)) || !(CompareNum(x2, adrs_test->x2Expected)))
     {
@@ -24,31 +23,21 @@ void RunTest (const struct CheckData *adrs_test)   //функция тестирования
 
 void CheckAllTests()         //функция с тестами
 {
-    const int nTests = 6;
     FILE *test_file
     test_file = fopen("TestNumbers.txt", "r")
 
-    FILE *fp;
-    FILE *fopen ("TestNumbers", "r")
+    //FILE *test_file;
+    //FILE *fopen ("TestNumbers", "r")
 
     int n = 0;
+    struct CheckData test;
+    int n = fscanf(test_file, "%lg %lg %lg %lg %lg %d %d \n", &test.a, &test.b, &test.c, &test.c, &test.x1Expected, &test.x2Expected, &test.nRootsExpected, &test.nTest);
+
     while (n == 7)
     {
         struct CheckData test;
-        int n = fscanf(test_file, "%lg %lg %lg %lg %lg %d %d \n", &test.a, &test.b, &test.c, &test.c, &test.x1Expected, &test.x2Expected, &test.nRootsExpected, &test.nTest);
+        int n = fscanf (test_file, "%lg %lg %lg %lg %lg %d %d \n", &test.a, &test.b, &test.c, &test.c, &test.x1Expected, &test.x2Expected, &test.nRootsExpected, &test.nTest);
         RunTest (&test);
     }
 
 }
-
-
-
-/*void CheckAllTests()         //функция с тестами
-{
-    const int nTests = 6;
-    struct CheckData test[nTests] = {{.a = 1.0, .b = 2.0, .c = 1.0, .x1Expected = -1.0, .x2Expected =  0.0, .nRootsExpected = ONE_ROOT,       .nTest = 1},
-                                     {.a = 1.0, .b = 2.0, .c = 3.0, .x1Expected =  0.0, .x2Expected =  0.0, .nRootsExpected = NO_ROOTS,       .nTest = 2},
-                                     {.a = 0.0, .b = 2.0, .c = 1.0, .x1Expected = -0.5, .x2Expected =  0.0, .nRootsExpected = ONE_ROOT,       .nTest = 3},
-                                     {.a = 1.0, .b = 3.0, .c = 2.0, .x1Expected = -1.0, .x2Expected = -2.0, .nRootsExpected = TWO_ROOTS,      .nTest = 4},
-                                     {.a = 0.0, .b = 0.0, .c = 0.0, .x1Expected =  0.0, .x2Expected =  0.0, .nRootsExpected = INFINITY_ROOTS, .nTest = 5},
-                                     {.a = 0.0, .b = 0.0, .c = 5.0, .x1Expected =  0.0, .x2Expected =  0.0, .nRootsExpected = NO_ROOTS,       .nTest = 6}};*/
