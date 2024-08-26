ebf6769c7b43a3aafddde8f1643bdd3b07df797a
Теренков Кирилл <terenkovkirill@yandex.ru>
2024-08-26 19:48:48 +0300

    NewTests

--- Tests1.cpp
+++ Tests1.cpp
@@ -25,16 +25,30 @@ void RunTest (const struct CheckData *adrs_test)   //функция тестирования
 void CheckAllTests()         //функция с тестами
 {
     const int nTests = 6;
+    FILE *test_file
+    test_file = fopen("TestNumbers.txt", "r")
+
+    FILE *fp;
+    FILE *fopen ("TestNumbers", "r")
+
+    int n = 0;
+    while (n == 7)
+    {
+        struct CheckData test;
+        int n = fscanf(test_file, "%lg %lg %lg %lg %lg %d %d \n", &test.a, &test.b, &test.c, &test.c, &test.x1Expected, &test.x2Expected, &test.nRootsExpected, &test.nTest);
+        RunTest (&test);
+    }
+
+}
+
+
+
+/*void CheckAllTests()         //функция с тестами
+{
+    const int nTests = 6;
     struct CheckData test[nTests] = {{.a = 1.0, .b = 2.0, .c = 1.0, .x1Expected = -1.0, .x2Expected =  0.0, .nRootsExpected = ONE_ROOT,       .nTest = 1},
                                      {.a = 1.0, .b = 2.0, .c = 3.0, .x1Expected =  0.0, .x2Expected =  0.0, .nRootsExpected = NO_ROOTS,       .nTest = 2},
                                      {.a = 0.0, .b = 2.0, .c = 1.0, .x1Expected = -0.5, .x2Expected =  0.0, .nRootsExpected = ONE_ROOT,       .nTest = 3},
                                      {.a = 1.0, .b = 3.0, .c = 2.0, .x1Expected = -1.0, .x2Expected = -2.0, .nRootsExpected = TWO_ROOTS,      .nTest = 4},
                                      {.a = 0.0, .b = 0.0, .c = 0.0, .x1Expected =  0.0, .x2Expected =  0.0, .nRootsExpected = INFINITY_ROOTS, .nTest = 5},
-                                     {.a = 0.0, .b = 0.0, .c = 5.0, .x1Expected =  0.0, .x2Expected =  0.0, .nRootsExpected = NO_ROOTS,       .nTest = 6}};
-
-    for (int i = 0; i < nTests; i++)
-    {
-        RunTest(test[i]);
-    }
-
-}
+                                     {.a = 0.0, .b = 0.0, .c = 5.0, .x1Expected =  0.0, .x2Expected =  0.0, .nRootsExpected = NO_ROOTS,       .nTest = 6}};*/
